b779d848d2cf2ea9f820ea03fc56d50db8540c0e
quocvuong <quocvuong.tqv97@gmail.com>
2018-12-24 01:36:17 +0700

    production4

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,6 +78,7 @@ Rails.application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+  config.web_console.development_only = false
 
   # Use a different logger for distributed setups.
   # require 'syslog/logger'
