a66cd19352ca6953116f3c1144101f7cbe9265a7
quocvuong <quocvuong.tqv97@gmail.com>
2019-01-18 16:22:13 +0700

    fix 6: add trigger into migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_14_144947) do
+ActiveRecord::Schema.define(version: 2019_01_18_091806) do
 
   create_table "admins", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
