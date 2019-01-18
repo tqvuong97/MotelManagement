e3879b862e3bbaef745769c308d4bd5e52fc0f6b
quocvuong <quocvuong.tqv97@gmail.com>
2019-01-18 17:30:07 +0700

    fix 8

--- app/controllers/backup_guests_controller.rb
+++ app/controllers/backup_guests_controller.rb
@@ -5,11 +5,15 @@ class BackupGuestsController < ApplicationController
       @backup_guests = BackupGuest.joins("inner join rooms on backup_guests.room_id = rooms.id")
                            .where("rooms.admin_id = ? and (backup_guests.name like ? or backup_guests.identitycard like ? or backup_guests.phonenumber like ?)",current_admin.id,"%#{params[:r]}%","#{params[:r]}%","%#{params[:r]}%")
                            .order('created_at desc')
-
     else
       @backup_guests = BackupGuest.joins("inner join rooms on backup_guests.room_id = rooms.id")
                            .where("rooms.admin_id = ?",current_admin.id).order('created_at desc')
 
     end
+  end
+
+  private
+  def backup_guest_params
+    params.require(:backup_guest).permit(:name, :birth, :address, :identitycard, :phonenumber, :image, :job, :notice, :room_id,:email,:deleted_at)
   end
 end
--- app/controllers/guests_controller.rb
+++ app/controllers/guests_controller.rb
@@ -58,7 +58,7 @@ class GuestsController < ApplicationController
   # DELETE /guests/1.json
   def destroy
     @g_bk = BackupGuest.find_by("name = '"+@guest.name+"' and created_at = '" +@guest.created_at.to_s+ "' and room_id = " +@guest.room.id.to_s)
-    @g_bk.deleted_at = Time.now.to_s
+    @g_bk.deleted_at = Time.now
     @g_bk.save
     @room = Room.find(@guest.room_id)
     @guest.destroy
